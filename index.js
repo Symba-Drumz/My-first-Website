28ca767d42c1f58795b9592f6b4c95439bfc9b2f
Alpha <161722017+Symba-Drumz@users.noreply.github.com>
2024-09-23 11:01:07 +0300

    Add file via upload
    This is an uploaded javascript file of the website

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop - 100,
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.drum-section').forEach(section => {
+    section.addEventListener('click', function() {
+        this.classList.toggle('clicked');
+    });
+});
